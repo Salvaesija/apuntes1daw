8b80268598a77ac20ab7ccfc2fc52458ebd0ed2b
Salvaesija <salvadorruizlozano@gmail.com>
2019-09-25 20:02:08 +0200

    prueba

---
+++ PROGRAMACIÓN/Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+	public static void main(String[]args){
+		System.out.print("helloooooooooooooo");
+		System.out.println("holaaaaaaaaaaaaaaa");
+		System.out.println("\n------------------");
+		System.out.println("\nhelloooooooooooooo");
+		System.out.println("helloooooooooooooo");		
+		System.out.println("helloooooooooooooo");
+	}
+}
